e5e7a2864fe768fb258e8445a4e7c60071ee96a7
zergolf1994 <zergolf1994@gmail.com>
2022-10-28 21:28:09 +0700

    Statistic Update

---
+++ modules/Mysql/Statistic.js
@@ -0,0 +1,75 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("./index");
+
+const Statistics = sequelize.define(
+  "statistics",
+  {
+    id: {
+      type: DataTypes.INTEGER(11),
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    uid: {
+      type: DataTypes.INTEGER(11),
+      defaultValue: 0,
+    },
+    slug: {
+      type: DataTypes.STRING(50),
+      defaultValue: "",
+    },
+    player: {
+      type: DataTypes.TEXT,
+    },
+    referer: {
+      type: DataTypes.TEXT,
+    },
+    token: {
+      type: DataTypes.STRING(40),
+      defaultValue: "",
+    },
+    os_name: {
+      type: DataTypes.STRING(255),
+    },
+    os_version: {
+      type: DataTypes.STRING(255),
+    },
+    bs_name: {
+      type: DataTypes.STRING(255),
+    },
+    bs_version: {
+      type: DataTypes.STRING(255),
+    },
+    bs_major: {
+      type: DataTypes.STRING(255),
+    },
+    client_ip: {
+      type: DataTypes.STRING(255),
+    },
+    user_agent: {
+      type: DataTypes.TEXT,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+    },
+    lastseenAt: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    indexes: [
+      {
+        unique: false,
+        fields: ["uid", "slug", "player", "referer", "token"],
+      },
+    ],
+  }
+);
+
+(async () => {
+  await Statistics.sync({ force: false });
+})();
+
+module.exports = Statistics;
